0b1017bf623582038ea43f5029989374b29c853a
borgified <borgified@gmail.com>
2019-11-07 12:53:05 -0800

    Create main.tf

---
+++ main.tf
@@ -0,0 +1,12 @@
+module "dashboard" {
+  source  = "borgified/dashboard/datadog"
+  version = "0.1.5"
+  # insert the 3 required variables here
+  api_key = var.api_key
+  app_key = var.app_key
+  prefix = var.prefix
+}
+
+output "url" {
+  value = module.dashboard.url
+}
