2a945b43309dd8b840d00eb8dbdd31bba2203e67
Juan Carlos Tong <juancarlos.tong@optimizely.com>
2019-11-07 23:08:29 -0800

    a

--- main.tf
+++ main.tf
@@ -1,25 +1,10 @@
-resource "null_resource" "step1" {
-  provisioner "local-exec" {
-    command = "curl -sL https://github.com/stedolan/jq/releases/download/jq-1.6/jq-linux64 -o jq && chmod u+x jq"
-  }
-}
-
-resource "null_resource" "step2" {
-  provisioner "local-exec" {
-    command = "./jq --help"
-  }
-  depends_on = [ null_resource.step1 ]
-}
-
-
 module "dashboard" {
   source  = "borgified/dashboard/datadog"
-  version = "0.1.7"
+  version = "0.1.8-beta"
   # insert the 3 required variables here
   api_key = var.api_key
   app_key = var.app_key
   prefix = var.prefix
-  description = null_resource.step2.id
 }
 
 output "url" {
